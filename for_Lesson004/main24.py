1564efb072edf1e1db9f0b0dd864561421bc9803
Ykiseliov <ykiseliov@gmail.com>
2023-05-24 23:55:35 +0300

    add decision

--- for_Lesson004/main24.py
+++ for_Lesson004/main24.py
@@ -1,2 +1,12 @@
 # Задача 24
 
+N = int(input("Кол-во кустов: "))
+a = list(map(int, input("Кол-во ягод на каждом кусте (чпб): ").split()))
+
+max_sum = 0
+
+for i in range(N):
+    sum_i = a[i] + a[(i-1)%N] + a[(i+1)%N]
+    max_sum = max(max_sum, sum_i)
+
+print("максимальное кол-во ягод", max_sum)
