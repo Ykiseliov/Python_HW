87441edbbdfca38da71c97211389d22e9fa92405
Ykiseliov <ykiseliov@gmail.com>
2023-06-03 19:08:31 +0300

    add main30.py

---
+++ for_Lesson006/main30.py
@@ -0,0 +1,6 @@
+a1 = int(input("Введите первый элемент прогрессии: "))
+d = int(input("Введите разность прогрессии: "))
+n = int(input("Введите количество элементов прогрессии: "))
+
+progression = [a1 + (i - 1) * d for i in range(1, n+1)]
+print(progression)
