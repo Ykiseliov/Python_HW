1c0cb06dbdb1a422af6cfc0763a6e18b0ad04d99
Ykiseliov <ykiseliov@gmail.com>
2023-06-07 19:48:58 +0300

    add main34.py & Descrip

--- for_Lesson007/main34.py
+++ for_Lesson007/main34.py
@@ -8,3 +8,20 @@
 
 # Ввод: пара-ра-рам рам-пам-папам па-ра-па-дам
 # Вывод: Парам пам-пам
+
+def bit(line):
+    line = line.split()
+    list_1 = []
+    for word in line:
+        sum_w = 0
+        for i in word:
+            if i in 'аеёиоуыэюя':
+                sum_w += 1
+        list_1.append(sum_w)
+    return len(list_1) == list_1.count(list_1[0])
+
+line_1 = input('Ведите текст для проверки: ')#'пара-ра-рам рам-пам-папам па-ра-па-дам'
+if bit(line_1):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
